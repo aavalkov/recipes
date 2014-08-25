28ec74901f309adf0e95761c4c03d9561b13f530
Anna Valkov & Nick Bernard <student@epicodus.com>
2014-08-25 10:00:17 -0700

    add functionality to create recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,19 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     render('recipes/index.html.erb')
   end
+
+  def new
+    @recipe = Recipe.new(params[:recipe])
+    render('recipes/new.html.erb')
+  end
+
+  def create
+    @recipe = Recipe.new(params[:recipe])
+    if @recipe.save
+      flash[:notice] = "Your recipe was added."
+      redirect_to("/recipes/#{@recipe.id}")
+    else
+      render('recipes/new.html.erb')
+    end
+  end
 end
